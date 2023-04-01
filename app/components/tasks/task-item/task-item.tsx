4b36c584b37ce7cab32d0d20dde696c8c8f74fa1
Wes Dollar <wes+github@dollardojo.tech>
2023-03-31 22:48:28 -0400

    ci: attempting to redeploy site... for reasons

--- app/components/tasks/task-item/task-item.tsx
+++ app/components/tasks/task-item/task-item.tsx
@@ -19,41 +19,42 @@ export const TaskItem = ({
   labels,
   taskId,
   statusId,
-}: LimitedTaskItemProps) => (
-  <Box
-    key={`task-${taskId}`}
-    backgroundColor={colors.background.body}
-    color={colors.text.primaryText}
-    marginBottom={"space40"}
-    padding={"space60"}
-    style={{
-      borderLeft: `16px solid ${
-        statusId === 1 ? colors.brandPrimary : colors.border.error
-      }`,
-    }}
-    _last={{ marginBottom: "space0" }}
-  >
-    {title && (
-      <Heading as="h2" variant="heading30">
-        {title}
-      </Heading>
-    )}
-    <Stack as="div" orientation={"horizontal"} spacing="space40">
-      <Box>
-        {/* {new Date(dueDate).toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        })} */}
-      </Box>
-      <Box>
-        <DisplayPillGroup aria-label="Task Item">
-          {labels.length &&
-            labels.map(({ name, id }) => (
-              <DisplayPill key={`labelPill-${id}`}>{name}</DisplayPill>
-            ))}
-        </DisplayPillGroup>
-      </Box>
-    </Stack>
-  </Box>
-);
+}: LimitedTaskItemProps) => {
+  const dueDateFormatted = new Date(dueDate).toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
+
+  return (
+    <Box
+      key={`task-${taskId}`}
+      backgroundColor={colors.background.body}
+      color={colors.text.primaryText}
+      marginBottom={"space40"}
+      padding={"space60"}
+      style={{
+        borderLeft: `16px solid ${
+          statusId === 1 ? colors.brandPrimary : colors.border.error
+        }`,
+      }}
+      _last={{ marginBottom: "space0" }}
+    >
+      {title && (
+        <Heading as="h2" variant="heading30">
+          {title}
+        </Heading>
+      )}
+      <Stack as="div" orientation={"horizontal"} spacing="space40">
+        <Box>{dueDateFormatted}</Box>
+        <Box>
+          <DisplayPillGroup aria-label="Task Item">
+            {labels.length &&
+              labels.map(({ name, id }) => (
+                <DisplayPill key={`labelPill-${id}`}>{name}</DisplayPill>
+              ))}
+          </DisplayPillGroup>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
